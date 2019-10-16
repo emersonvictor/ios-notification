82bc944d3e9954b2d6f5d0625974ebab63d48233
Emerson Victor <emersonvvictor@gmail.com>
2019-10-16 11:04:33 -0300

    Create notification function

--- notification/notification/Managers/LocalNotificationManager.swift
+++ notification/notification/Managers/LocalNotificationManager.swift
@@ -13,6 +13,7 @@ class LocalNotificationManager: NSObject, UNUserNotificationCenterDelegate {
     
     private let notificationCenter = UNUserNotificationCenter.current()
     
+    // MARK: - Notification authorization
     func requestAuthorization(withOptions options: UNAuthorizationOptions) {
         self.notificationCenter.requestAuthorization(options: options) { (didAllow, error) in
             /// User permission
@@ -31,5 +32,19 @@ class LocalNotificationManager: NSObject, UNUserNotificationCenterDelegate {
         self.notificationCenter.getNotificationSettings { (setting) in
             completion(setting.authorizationStatus)
         }
+    }
+    
+    // MARK: - Creating notifications
+    private func scheduleNotification(withTitle title: String, subtitle: String, body: String, badge: Int) -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        
+        /// Set notification content
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.badge = badge as NSNumber
+        content.sound = .default
+        
+        return content
     }
 }
